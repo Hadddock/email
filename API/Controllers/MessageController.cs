28a29a4f22084d75e552f53d7a749ae9e6baa419
Justin Haddock <jhadddock@protonmail.com>
2022-12-02 15:26:55 -0800

    Implement Web API

---
+++ API/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using API.DTOs;
+using Message;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MessageController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<MessageController> _logger;
+
+    public MessageController(ILogger<MessageController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost(Name = "SendEmail")]
+    public string Post(MessageDto messageDto)
+    {
+        if
+        (
+            messageDto.messageBody is not null &&
+            messageDto.messageSubject is not null &&
+            messageDto.recipientEmail is not null
+        )
+
+        {
+            Message.Program.Main(new string[] { messageDto.recipientEmail, messageDto.messageSubject, messageDto.messageBody });
+        }
+
+        return "test";
+    }
+}
---
+++ API/DTOs/MessageDto.cs
@@ -0,0 +1,10 @@
+namespace API.DTOs
+{
+    public class MessageDto
+    {
+        public string? recipientEmail { get; set; }
+        public string? messageSubject { get; set; }
+        public string? messageBody { get; set; }
+        
+    }
+}
